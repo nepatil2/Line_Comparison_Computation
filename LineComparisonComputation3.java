00db4b58124019a5ba8d327c2318eb386da44c3d
nepatil2 <nehapatil22899@gmail.com>
2021-12-07 14:50:02 +0530

    Added Line Comparison Methods

--- LineComparisonComputation3.java
+++ LineComparisonComputation3.java
@@ -3,8 +3,8 @@ package com.bridgelabz.practice_problem;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class LineComparisonComputation2 {
-    public LineComparisonComputation2(int x1, int y1, int x2, int y2){
+public class LineComparisonComputation3 {
+    public LineComparisonComputation3(int x1, int y1, int x2, int y2){
 
     }
 
@@ -20,13 +20,24 @@ public class LineComparisonComputation2 {
         System.out.print("Enter Length of Y2 Co-Ordinate :");
         int y2 = sc.nextInt();
 
-        LineComparisonComputation2 a =new LineComparisonComputation2(x1,y1,x2,y2);
+        LineComparisonComputation3 a =new LineComparisonComputation3(x1,y1,x2,y2);
         double line1 = a.LengthOfLine1(x1,y1);
         System.out.println("Length Of Line1 : "+line1);
         double line2 = a.LengthOfLine1(x2,y2);
         System.out.println("Length Of Line2 : "+line2);
 
         System.out.println(Objects.equals(line1,line2));
+        boolean l1 = a.compareTo((int) line1,(int) line2);
+        boolean l2 = a.compareTo1((int) line1,(int) line2);;
+        if(l1){
+            System.out.println("Line One Is Greater Than Line Two");
+        }
+        else if (l2){
+            System.out.println("Line One Is Lesser Than Line Two");
+        }
+        else{
+            System.out.println("Both Lines Are Equal");
+        }
 
     }
     public double LengthOfLine1(int x1, int y1){
@@ -37,5 +48,13 @@ public class LineComparisonComputation2 {
     public double LengthOfLine2(int x2, int y2){
         double length2 = Math.sqrt((x2-y2)*(x2-y2));
         return length2;
+    }
+    public boolean compareTo(int line1 , int line2){
+        boolean l1 =(line1 > line2);
+        return l1;
+    }
+    public boolean compareTo1(int line1 , int line2){
+        boolean l2 =(line1 < line2);
+        return l2;
     }
 }
